e355fda1d3cf7e53809442daa9e2c3817b823a45
Frances Botsford <frances@switchingprotocols.com>
2023-03-30 12:28:04 -0400

    update outlook text color on button for dark mode

--- test/faButton.js
+++ test/faButton.js
@@ -16,10 +16,12 @@ describe("FAButton", () => {
     strokecolor="#1e3650"
     fillcolor="#63E6BE"><w:anchorlock /><center
       style="
-        color: #183153;
+        color: #DBE5F0;
         font-family: sans-serif;
         font-size: 16px;
         font-weight: bold;
+        mso-style-textfill-type:gradient;
+        mso-style-textfill-fill-gradientfill-stoplist:'0 \#183153 1 100000\,99000 \#183153 1 100000';
       "
     >
       Some New Button Text
